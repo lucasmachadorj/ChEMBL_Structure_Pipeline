3826002871a36bbd773c2ca3b44874344498c8b2
Lucas Machado <lucasmachado.rj@gmail.com>
2022-07-04 13:54:03 -0400

    hardcoded version

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
-import chembl_structure_pipeline
 
 setup(name='chembl_structure_pipeline',
-      version=chembl_structure_pipeline.__version__,
+      version="1.0.0",
       description='ChEMBL Structure Pipeline',
       url='https://www.ebi.ac.uk/chembl/',
       author='Greg Landrum',
